dc6d5ca4c97abb92de021e749efa2e55f5987479
Yakanaro <kipaherd@gmail.com>
2022-09-19 17:45:35 +0300

    db

--- knexfile.js
+++ knexfile.js
@@ -26,10 +26,20 @@ export const test = {
   migrations,
 };
 
+// export const production = {
+//   client: 'sqlite3',
+//   connection: {
+//     filename: './database.sqlite',
+//   },
+//   useNullAsDefault: true,
+//   migrations,
+// };
+
 export const production = {
-  client: 'sqlite3',
+  client: 'pg',
   connection: {
-    filename: './database.sqlite',
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
   },
   useNullAsDefault: true,
   migrations,
